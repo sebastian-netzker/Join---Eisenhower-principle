28257fcd6c451caa3ef49d1cccbd1a46c86a18bb
SimonBesendorfer <simon.besendorfer@me.com>
2020-09-14 13:41:18 +0200

    fixed empty json

--- scriptSimon.js
+++ scriptSimon.js
@@ -1,3 +1,5 @@
+let allTasks =[];
+
 function emptyFields(){
     document.getElementById("title").value = null;
     document.getElementById("urgency").value = null;
@@ -9,6 +11,33 @@ function emptyFields(){
 
 
 function addTask(){
-    let tasks = JSON.parse(tasks);
-    console.log(tasks);
+    let title = document.getElementById("title").value;
+    let urgency = document.getElementById("urgency").value;
+    let importance = document.getElementById("importance").value;
+    let date = document.getElementById("date").value;
+    let discription = document.getElementById("description").value;
+
+    let task = {
+        'title': title,
+        'urgency': urgency,
+        'importance': importance,
+        'date': date,
+        'discription': discription,
+        'created': new Date().getTime() //unix timestamp at the time of creating the task 
+    }
+   
+    allTasks.push(task);
+
+    tasksString = JSON.stringify(allTasks);
+    localStorage.setItem('data', tasksString);
+}
+
+function loadTasks(){
+    if (localStorage.getItem('data') == null){ //if ther is no existing 'data' file in localStorage it will be created
+        let empty;
+        localStorage.setItem('data', empty);
+    } else {
+    let tasksString = localStorage.getItem('data');
+    allTasks = JSON.parse(tasksString);
+    }
 }
