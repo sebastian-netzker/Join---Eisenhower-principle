17ce31d917c36cadc55bcc3fefa45a0c1120c4b5
Sebastian Netzker <you@example.com>
2020-09-18 12:14:55 +0200

    add different color to each field in matrix

--- scriptSebastian.js
+++ scriptSebastian.js
@@ -1,90 +1,95 @@
-let generatedHTMLCode ; 
+let generatedHTMLCode;
 
+let html;
 
+let i;
 
+let list1;
+
+let list2;
+
+let list3;
+
+let list4;
+
+let list;
 
 function insertTasktoMatrix() {
-  let list1 = document.getElementById("list-important-urgent");
-  let list2 = document.getElementById("list-important-noturgent");
-  let list3 = document.getElementById("list-notimportant-urgent");
-  let list4 = document.getElementById("list-notimportant-noturgent");
+  list1 = document.getElementById("list-important-urgent");
+  list2 = document.getElementById("list-important-noturgent");
+  list3 = document.getElementById("list-notimportant-urgent");
+  list4 = document.getElementById("list-notimportant-noturgent");
 
-  
-
-
-  for (let i = 0; i < allTasks.length; i++) {
-
-
-      generatedHTMLCode = generateListItem(
-        allTasks[i].date,
-        allTasks[i].title,
-        allTasks[i].description
-      );
-
+  for (i = 0; i < allTasks.length; i++) {
     if (
       allTasks[i].importance == "High Importance" &&
       allTasks[i].urgency == "High Urgency"
     ) {
-
-       
-      list1.insertAdjacentHTML('beforeend', generatedHTMLCode);
-      
-
-      
+      createFieldinList1(1);
     } else if (
       allTasks[i].importance == "High Importance" &&
       allTasks[i].urgency == "Low Urgency"
     ) {
-
-       
-
-
-  
-      list2.insertAdjacentHTML('beforeend', generatedHTMLCode);
+      createFieldinList2();
     } else if (
       allTasks[i].importance == "Low Importance" &&
       allTasks[i].urgency == "High Urgency"
     ) {
-      
-      list3.insertAdjacentHTML('beforeend', generatedHTMLCode);
+      createFieldinList3();
     } else if (
       allTasks[i].importance == "Low Importance" &&
       allTasks[i].urgency == "Low Urgency"
     ) {
-
-
-      list4.insertAdjacentHTML('beforeend', generatedHTMLCode);
+      createFieldinList4();
     }
   }
 }
 
-function generateListItem(date,title,description) {
+function createFieldinList1() {
+  m = 1;
+  generateHTMLCode();
+  list1.insertAdjacentHTML("beforeend", generatedHTMLCode);
+}
 
-  
+function createFieldinList2() {
+  m = 2;
+  generateHTMLCode();
+  list2.insertAdjacentHTML("beforeend", generatedHTMLCode);
+}
 
-  let html = 
-    `<li  >
+function createFieldinList3() {
+  m = 3;
+  generateHTMLCode();
+  list3.insertAdjacentHTML("beforeend", generatedHTMLCode);
+}
+
+function createFieldinList4() {
+  m = 4;
+  generateHTMLCode();
+  list4.insertAdjacentHTML("beforeend", generatedHTMLCode);
+}
+
+function generateListItem(m, date, title, description) {
+  html = `<li class="li-${m}">
       <img onclick="deleteTask()" class="garbage_can" src="img/garbage_can.png">
       <span class="date">${date}</span> <br> 
       <span class="title">${title}</span> <br> 
       <span class="description">${description}
     </li>`;
-    
 
-    return html;
-     
-
+  return html;
 }
 
-function deleteTask(){
+function generateHTMLCode() {
+  generatedHTMLCode = generateListItem(
+    m,
+    allTasks[i].date,
+    allTasks[i].title,
+    allTasks[i].description
+  );
+}
 
-
-  
+function deleteTask() {
   localStorage.clear();
   location.reload();
-
- 
-
 }
-
-
