23b540cf74dee1e2567b8107be3efbe36c4d3b13
SimonBesendorfer <simon.besendorfer@me.com>
2020-09-18 14:18:13 +0200

    changed popup design, splited function

--- scriptSimon.js
+++ scriptSimon.js
@@ -1,5 +1,7 @@
 let allTasks =[];
-
+/**
+ * This function will empty all entries in the add Task form. The previous added Information will not be changed
+ */
 function emptyFields(){
     document.getElementById("title").value = null;
     document.getElementById("urgency").value = "High Urgency";
@@ -8,11 +10,10 @@ function emptyFields(){
     document.getElementById("description"). value = null;
 }
 
-
-
-
-
-function addTask(){
+/**
+ * creates the JSON 'task' which is used in the function addTask() for saving the data in the local storage
+ */
+function gererateTaskObject(){
     let title = document.getElementById("title").value;
     let urgency = document.getElementById("urgency").value;
     let importance = document.getElementById("importance").value;
@@ -26,31 +27,33 @@ function addTask(){
         'date': date,
         'description': discription,
         'created': new Date().getTime() //unix timestamp at the time of creating the task 
-    }
+    };
+    return task;
+}
    
-    allTasks.push(task);
+/**
+ * This function will add the entries in the add Task form to the JSON allTasks
+ */
+function addTask(){
+    let t = gererateTaskObject();
+    allTasks.push(t);
 
     tasksString = JSON.stringify(allTasks);
     localStorage.setItem('data', tasksString);
     emptyFields();
-    // alert('A new Task with the Title '+ title + ' has been added!');
-    let html = "<div class='popup' id='popup'><h5>A new Task with the Title " + title +" has been added!</h5>You have set " + importance + " and " + urgency + "!</div>";
+    let html = "<div id='popup' class='transparentgray'><div class='popup'><h5>A new Task with the Title " + title +" has been added!</h5>You have set " + importance + " and " + urgency + "!</div></div>";
     document.getElementById("mainWindow").insertAdjacentHTML('beforeEnd', html);
     setTimeout(function (){
         document.getElementById('popup').remove();
-    } ,2000);
-    
+    } ,4000);
 }
 
-function removePopup(){
-        let popup = document.getElementById('popup');
-        popup.remove();
-}
-
+/**
+ * loads the JSON allTasks from local storage, if ther is no 'data' it will be created
+ */
 function loadTasks(){
     if (localStorage.getItem('data') == null){ //if ther is no existing 'data' file in localStorage it will be created
-        let empty;
-        localStorage.setItem('data', empty);
+        localStorage.setItem('data', JSON.stringify([]));
     } else {
     let tasksString = localStorage.getItem('data');
     allTasks = JSON.parse(tasksString);
