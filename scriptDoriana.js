724331ff37d5c3e2233915ba5e23d7890e44ec17
Doriana <dorina.mercea@yahoo.de>
2020-09-22 11:10:14 +0200

    list design

---
+++ scriptDoriana.js
@@ -0,0 +1,52 @@
+let priorityStatus = "high";
+let status = 0;
+let listArray = [];
+function createItemsToTheList(title, priorityStatus, discription, i) {
+    console.log('create:' + i);
+    let item = `
+        <li class="item" id="item${i}">
+       
+        <p> ${title}</p>
+        <p> ${priorityStatus}</p>
+        <p> ${discription}</p>
+        <button onclick="deleteItemList(${i})"class="delete">Delete Task</button>
+        </li>
+         `;
+
+    return item;
+
+
+
+}
+
+function insertItemsOnTheList() {
+
+    for (let i = 0; i < allTasks.length; i++) {
+        if (allTasks[i].importance == "High Importance" && allTasks[i].urgency == "High Urgency") {
+            priorityStatus = "High";
+        }
+        else if (allTasks[i].importance == "Low Importance" && allTasks[i].urgency == "Low Urgency") {
+            priorityStatus = "Low";
+        }
+        else {
+            priorityStatus = "Medium";
+        }
+        let insertItem = createItemsToTheList(allTasks[i].title, priorityStatus, allTasks[i].description, i);
+        document.getElementById('listItem1').insertAdjacentHTML('beforeend', insertItem);
+
+    }
+}
+
+function deleteItemList(id) {
+    let item = document.getElementById('item' + id);
+    item.parentNode.removeChild(item);
+    allTasks.splice(id, 1);
+
+
+}
+
+
+
+
+
+
